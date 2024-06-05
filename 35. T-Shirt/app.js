a19993096e4061890461fd845c3adb286d160a76
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-05 13:34:20 -0700

    Add files via upload

---
+++ 35. T-Shirt/app.js
@@ -0,0 +1,4 @@
+function make_shirt(size, text) {
+    console.log("A ".concat(size, " size t-shirt with the text: ").concat(text, "."));
+}
+make_shirt("small", "love laugh live");
---
+++ 35. T-Shirt/app.ts
@@ -0,0 +1,5 @@
+function make_shirt(size: string, text: string){
+    console.log(`A ${size} size t-shirt with the text: ${text}.`);
+}
+
+make_shirt("small", "love laugh live")
